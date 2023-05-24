facdedcfa7569af3ce61dc4d47f71c518fa03860
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2023-05-25 00:30:34 +0530

    Update index.js

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -5,7 +5,7 @@ import UserSearch from "./components/UserSearch";
 import UsersList from "./components/UsersList";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:5000');
+const socket = io('http://chatty.onrender.com');
 localStorage.getItem('socket', socket)
 function Home() {
   const [searchKey, setSearchKey] = React.useState("");
