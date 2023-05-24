9d45c0ba6e5d1db005d9df1b2e554cdf97f72330
Pritam Chatterjee <cpritam387@gmail.com>
2023-05-25 00:19:16 +0530

    first commit

---
+++ server/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: "dcnvxk807",
+  api_key: "315896177865129",
+  api_secret: "co7XOR93edDzT9aUP815VEA8qvg",
+});
+
+module.exports = cloudinary;
