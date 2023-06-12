7ed508a8c59daadd36b82239c1626ee8036d6080
Pritam Chatterjee <74919879+Pritam2023@users.noreply.github.com>
2023-06-12 11:59:50 +0530

    Update index.js

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -5,7 +5,7 @@ import UserSearch from "./components/UserSearch";
 import UsersList from "./components/UsersList";
 import { io } from "socket.io-client";
 
-const socket = io('http://localhost:5000');
+const socket = io('https://fyp-chatty.onrender.com');
 function Home() {
   const [searchKey, setSearchKey] = React.useState("");
   const { selectedChat, user } = useSelector((state) => state.userReducer);
